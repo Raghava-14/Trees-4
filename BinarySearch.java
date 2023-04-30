fe49aeb57cfd8e94e7486395ebde505f4126dd50
Raghava-14 <73310160+Raghava-14@users.noreply.github.com>
2023-04-30 02:05:28 -0500

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,16 @@
+//Time = 
+//Space = O(h), h is height of the binary search tree
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null; // Base case
+
+        if (p.val < root.val && q.val < root.val) { // p and q are both in the left subtree
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (p.val > root.val && q.val > root.val) { // p and q are both in the right subtree
+            return lowestCommonAncestor(root.right, p, q);
+        } else { // p and q are on different sides of the root
+            return root;
+        }
+    }
+}
